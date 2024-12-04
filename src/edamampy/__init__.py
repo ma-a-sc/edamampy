dc8ce682cc0c7df102a9b09bf834d971ec7b6d86
Mark Scharmann <mark.scharmann@fintiba.com>
2024-12-04 16:02:42 +0100

    lets try again

--- src/edamampy/__init__.py
+++ src/edamampy/__init__.py
@@ -1,3 +1,12 @@
+"""
+Edamampy
+
+A library to make the interaction with the Edamam recipe search api easier.
+"""
+
+__version__ = "0.0.4"
+__author__ = 'Mark Scharmann'
+
 import classes
 import config
 import constants
