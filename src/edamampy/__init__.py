69c50dee8819c4fb1ad6d8c8c9ec7c52edf0c961
Mark Scharmann <mark.scharmann@fintiba.com>
2024-12-19 14:45:45 +0100

    redo versioning

--- src/edamampy/__init__.py
+++ src/edamampy/__init__.py
@@ -4,7 +4,7 @@ Edamampy
 A library to make the interaction with the Edamam recipe search api easier.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.1"
 __author__ = "Mark Scharmann"
 
 from .classes import EdamamAPIHandler
